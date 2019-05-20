1798d55ca79825ed6e2285a2d16e581ad92045c7
stanislawkostenko <rng92@mail.ru>
2019-05-20 13:09:49 +0300

    поправил и выполнил второе заданиел

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,14 +1,13 @@
 const image = document.getElementById('cookie');
-const number = Number(document.getElementById('clicker__counter'));
-//const number = Number.parseInt(document.getElementById('clicker__counter'));
-//При клике получается не суммирование а строковая склейка. Преобразование в число не работает
+const number = document.getElementById('clicker__counter');
 
 const changeWidthAndNumber = function() {
-    number.textContent += 1;
+    number.textContent = parseInt(number.textContent) + 1;
     if (number.textContent % 2 !== 0) {
       image.width = 250;
+    } else {
+      image.width = 200;
     }
-    image.width = 200;
 }
 
 image.onclick = changeWidthAndNumber;
