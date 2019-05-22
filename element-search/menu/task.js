3e9cdec02d808a0e2bd6943f90710ed26735c448
stanislawkostenko <rng92@mail.ru>
2019-05-22 21:28:24 +0300

    второе задание

---
+++ element-search/menu/task.js
@@ -0,0 +1,7 @@
+const menuItem = document.getElementsByClassName('menu_sub').add(menu_active);
+//menuItem.className = 'menu menu_sub menu_active';
+const linkItem = document.getElementsByClassName('menu__link');
+
+menuItem.onclick;
+
+//СНОВА ЗАПУТАЛСЯ. ПО ИДЕЕ ДОЛЖНО ПОЯВЛЯТЬСЯ ПОДМЕНЮ НО ОНО НЕ ПОЯВЛЯЕТСЯ И Я НЕ ПОНИМАЮ ПОЧЕМУ. НЕ МОГУ ПОКА ДАЛЬШЕ ВЫПОЛНЯТЬ ЗАДАНИЕ.
