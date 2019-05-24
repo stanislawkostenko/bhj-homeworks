6f93c9e05bbdeb314d5e4c24d77f552bc2de0024
stanislawkostenko <rng92@mail.ru>
2019-05-24 12:26:51 +0300

    снова поправил первое задание. убрал первый попап

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -4,8 +4,9 @@ firstPopup.className = 'modal modal_active';
 const successPoppup = document.getElementById('modal_success');
 
 const secondPopup = document.querySelector('div a.btn_danger');
-secondPopup.onclick = function () { successPoppup.className = 'modal modal_active'; }
+secondPopup.onclick = function () { firstPopup.className = 'modal';
+                                    successPoppup.className = 'modal modal_active'; }
 
 const closePopup = document.getElementsByClassName('modal__close_times');
 closePopup[0].onclick = function () { firstPopup.className = 'modal'; }
-closePopup[1].onclick = function () { successPoppup.className = 'modal'; firstPopup.className = 'modal'; }
+closePopup[1].onclick = function () { successPoppup.className = 'modal'; }
