36a70e676853362490e15fc7a2151e09f85aa992
stanislawkostenko <rng92@mail.ru>
2019-05-29 16:31:50 +0300

    поправил dropdown

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,14 +1,14 @@
-const adversity = document.querySelectorAll('rotator__case');
+const adversity = document.querySelectorAll('.rotator__case');
 
 const changeAds = function() {
-    const parentElement = this.closest('.rotator__case');
+    const parentElement = document.querySelector('.rotator');
     for (let el of adversity) {
         if (parentElement.firstElementChild.classList.contains('rotator__case_active')) {
             parentElement.firstElementChild.classList.remove('rotator__case_active');
             parentElement.firstElementChild.nextElementSibling.classList.add('rotator__case_active');
         } else if (el.classList.contains('rotator__case_active')) {
             el.classList.remove('rotator__case_active');
-            el.nextElementSibling.classList.add('rotator__case_active');
+            //el.nextElementSibling.classList.add('rotator__case_active');
         } else if (parentElement.lastElementChild.classList.contains('rotator__case_active')) {
             parentElement.lastElementChild.classList.remove('rotator__case_active');
             parentElement.firstElementChild.classList.add('rotator__case_active');
--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,28 +1,15 @@
 const dropdownList = document.querySelector('.dropdown__list');
 const dropdownBtn = document.querySelector('.dropdown__value');
-const dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'));
+const dropdownItems = document.querySelectorAll('.dropdown__item');
 
-const handler1 = () => { dropdownList.classList.add('dropdown__list_active'); }
-dropdownBtn.addEventListener('click', handler1);
+dropdownBtn.addEventListener('click', function() {
+    dropdownList.classList.toggle('dropdown__list_active');
+});
 
-const fnc = (event) => {
-  if (dropdownItem[0].onclick) {
-    dropdownList.classList.remove('dropdown__list_active');
-    dropdownBtn.textContent = 'JavaScript';
-    return false;
-  } else if (dropdownItem[1].onclick) {
-    dropdownList.classList.remove('dropdown__list_active');
-    dropdownBtn.textContent = 'PHP';
-    return false;
-  } else if (dropdownItem[2].onclick) {
-    dropdownList.classList.remove('dropdown__list_active');
-    dropdownBtn.textContent = 'Python';
-    return false;
-  } else if (dropdownItem[3].onclick) {
-    dropdownList.classList.remove('dropdown__list_active');
-    dropdownBtn.textContent = 'Fortran';
-    return false;
-  }
+for (let item of dropdownItems) {
+    item.addEventListener('click', function(event) {
+        dropdownBtn.textContent = event.target.textContent;
+        dropdownList.classList.remove('dropdown__list_active');
+        event.preventDefault();
+    });
 }
-
-dropdownItem.addEventListener('click', fnc);
