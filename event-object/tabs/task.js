0373b05e3d2d7bc306c3d9adc8acfd2f86186b5f
stanislawkostenko <rng92@mail.ru>
2019-05-29 17:22:17 +0300

    поправил tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,42 +1,13 @@
-const tab = Array.from(document.getElementsByClassName('tab'));
-const content = Array.from(document.getElementsByClassName('tab__content'));
+const tabs = Array.from(document.querySelectorAll('.tab'));
+const contents = document.querySelectorAll('.tab__content');
 
-tab[0].onclick = () => { if (tab[0].classList.contains('tab_active')) {
-                           return false;
-                         } else {
-                           tab[0].classList.add('tab_active');
-                           tab[1].classList.remove('tab_active');
-                           tab[2].classList.remove('tab_active');
-                           content[0].classList.add('tab__content_active');
-                           content[1].classList.remove('tab__content_active');
-                           content[2].classList.remove('tab__content_active');
-                         }
-                         return false;
-                       }
-
-
-tab[1].onclick = () => { if (tab[1].classList.contains('tab_active')) {
-                           return false;
-                         } else {
-                           tab[1].classList.add('tab_active');
-                           tab[0].classList.remove('tab_active');
-                           tab[2].classList.remove('tab_active');
-                           content[1].classList.add('tab__content_active');
-                           content[0].classList.remove('tab__content_active');
-                           content[2].classList.remove('tab__content_active');
-                         }
-                         return false;
-                      }
-
-tab[2].onclick = () => { if (tab[2].classList.contains('tab_active')) {
-                           return false;
-                         } else {
-                           tab[2].classList.add('tab_active');
-                           tab[0].classList.remove('tab_active');
-                           tab[1].classList.remove('tab_active');
-                           content[2].classList.add('tab__content_active');
-                           content[0].classList.remove('tab__content_active');
-                           content[1].classList.remove('tab__content_active');
-                         }
-                         return false;
-                       }
+for (let tab in tabs) {
+    tabs[tab].addEventListener('click', function(event) {
+        for (let i in tabs) {
+            tabs[i].classList.remove('tab_active');
+            contents[i].classList.remove('tab__content_active');
+        }
+        event.target.classList.add('tab_active');
+        contents[tabs.indexOf(event.target)].classList.add('tab__content_active');
+    });
+}
