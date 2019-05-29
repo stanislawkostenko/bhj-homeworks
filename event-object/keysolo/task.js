56572a095b18616d7241fccb40ff3f46d90dd0cd
stanislawkostenko <rng92@mail.ru>
2019-05-29 17:30:43 +0300

    1

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,6 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
 
      document.addEventListener('keydown', event => {
          const screenLetter = this.currentSymbol.textContent.toLowerCase().charCodeAt(0);
@@ -34,7 +27,6 @@ class Game {
              this.fail();
          }
      });
-
 
   }
 
