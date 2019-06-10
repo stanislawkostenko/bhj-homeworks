342c56ee884ea5b2aa338291879afdd7eb95bce2
stanislawkostenko <rng92@mail.ru>
2019-06-10 13:20:04 +0300

    поправил tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,14 +1,18 @@
 const buttons = document.querySelectorAll('.has-tooltip');
+const tooltip = document.querySelector('.tooltip');
 
 for (let button of buttons) {
     button.addEventListener('click', (event) => {
-        event.preventDefault();
-        const tooltipActive = document.querySelector('.tooltip_active');
-        if (tooltipActive) {
-            tooltipActive.classList.remove('tooltip_active');
-        }
-        event.target.nextSibling.classList.add('tooltip_active');
-    });
-    const tooltip = `<div class="tooltip">${button.title}</div>`;
-    button.insertAdjacentHTML('afterEnd', tooltip);
-}
+        event.preventDefault();const title = event.target.getAttribute('title');
+        const y = event.target.getBoundingClientRect().top;
+	      const x = event.target.getBoundingClientRect().left;
+	      tooltip.textContent = title;
+	      tooltip.style.display = 'block';
+	      tooltip.style.top = `${y + 20}px`;
+	      tooltip.style.left = `${x}px`;
+     });
+   }
+
+document.addEventListener('scroll', () => {
+	tooltip.style.display = 'none';
+});
